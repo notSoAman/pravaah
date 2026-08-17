a19223116f565f87b6763a349b46fad7c1502a78
Aman Singh Choudhary <amansinghchoudhary@protonmail.com>
2026-08-17 16:01:33 +0530

    removed initial data

--- config/settings.py
+++ config/settings.py
@@ -76,13 +76,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
+import dj_database_url
+
 # ─── Database ─────────────────────────────────────────────────────────────────
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 # ─── Password Validation ──────────────────────────────────────────────────────
